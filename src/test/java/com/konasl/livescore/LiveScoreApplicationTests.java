e885a25cc6f4131c574c31421ad8b011a04924c1
Mohammad Helal Khan <m.helal.k@gmail.com>
2022-09-30 02:02:14 +0600

    first commit

---
+++ src/test/java/com/konasl/livescore/LiveScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konasl.livescore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
